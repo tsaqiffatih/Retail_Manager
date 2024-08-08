d668bd8349e1abbc71c9ff8acdbc42f73f33ab5e
tsaqiffatih <tsaqiffatih92@gmail.com>
2024-08-08 10:36:52 +0700

    update router

--- src/routers/payrollRouter.ts
+++ src/routers/payrollRouter.ts
@@ -1,6 +1,10 @@
 import express from "express";
 import {  } from "../controllers/payrollController";
+import { auditMiddleware } from "../middleware/auditMiddleware";
 const router = express.Router();
+
+
+router.use(auditMiddleware("Store"));
 
 router.get("/:id", )
 
